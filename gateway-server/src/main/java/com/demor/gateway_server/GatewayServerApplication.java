ad335bfb6b5d7ee47ce3794a457b11987ae0f8b6
Dennis Moradkhani <dennis.moradkhani@gmail.com>
2026-02-21 18:54:55 +0100

    add spring-cloud gateway server

---
+++ gateway-server/src/main/java/com/demor/gateway_server/GatewayServerApplication.java
@@ -0,0 +1,46 @@
+package com.demor.gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	// Custom routing requirements
+	@Bean
+	public RouteLocator bankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder
+				.routes()
+				.route(p -> p
+						.path("/bank/accounts/**")
+						.filters(f -> f
+								.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/bank/loans/**")
+						.filters(f -> f
+								.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/bank/cards/**")
+						.filters(f -> f
+								.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
+						.uri("lb://CARDS"))
+				.build();
+	}
+}
